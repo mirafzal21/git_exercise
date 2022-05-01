e050f0ac676fff3e43417fd234a5c89a86a4eb06
mirafzal21 <104599189+mirafzal21@users.noreply.github.com>
2022-04-30 23:06:05 -0400

    [mirafzal] fixed failed issue

--- sample1.py
+++ sample1.py
@@ -4,3 +4,6 @@ for i in range(10):
     print(f"numbers {i}")
 
 print("New changes after last push, fixed PROJ-23455")
+
+print("this line is for PROJ-23455 test failes")
+print("applied new changes to fix the failed issue:)
