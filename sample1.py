c5e8a986ac0b670a413b18c0ec30ff2ad0d2cc5a
mirafzal <mirafzal.m21@gmail.com>
2022-05-01 00:08:58 -0400

    Added login scenario 1

--- sample1.py
+++ sample1.py
@@ -7,4 +7,4 @@ for i in range(10):
 print("New changes after last push, fixed PROJ-23455")
 
 print("this line is for PROJ-23455 test files")
-print("applied new changes to fix the failed issue:")
+print("applied new changes to fix the failed issue :")
