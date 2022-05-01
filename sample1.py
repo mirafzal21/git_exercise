c327e6289c0cf11bdab8353c04d77add432dd0be
mirafzal <mirafzal.m21@gmail.com>
2022-04-30 23:17:48 -0400

    typo fixed

--- sample1.py
+++ sample1.py
@@ -1,9 +1,10 @@
 # sample file for git
 print("hello world!")
+print("more lines in the sample1")
 for i in range(10):
-    print(f"numbers {i}")
+    print(f"numbers : {i}")
 
 print("New changes after last push, fixed PROJ-23455")
 
-print("this line is for PROJ-23455 test failes")
-print("applied new changes to fix the failed issue:)
+print("this line is for PROJ-23455 test files")
+print("applied new changes to fix the failed issue:")
